be8c16906244534bd32e8ca26769fdd9176257e9
agijsg <72884679+agijsg@users.noreply.github.com>
2021-05-20 10:34:50 +0200

    Delete jwttoken.py

--- jwttoken.py
+++
@@ -1,9 +0,0 @@
-import jwt
-
-encoded = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6Imd1ZXN0In0.OnuZnYMdetcg7AWGV6WURn8CFSfas6AQej4V9M13nsk"
-
-word_list = open('password_list.txt','r').read().split('\n')
-
-for word in word_list : 
-	secret = word
-	print(jwt.decode(encoded, secret, algorithms=['HS256']))
